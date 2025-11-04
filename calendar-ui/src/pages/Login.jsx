d1eacd7f254b60c93f835774c0a40479dd5fa949
ottokyt20 <otto.kytomaki@tuni.fi>
2025-11-04 11:37:27 +0200

    jako calendar.jsx ja app.jsx, loginille pohja

---
+++ calendar-ui/src/pages/Login.jsx
@@ -0,0 +1,8 @@
+export default function Login() {
+    return (
+        <div style={{ padding: 20 }}>
+            <h2>Kirjautumissivu</h2>
+            <p>Tähän tulee mock-login</p>
+        </div>
+    );
+}

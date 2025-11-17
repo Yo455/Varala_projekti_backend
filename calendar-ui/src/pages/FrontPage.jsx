b828f1260e30cfa47387713657f5c5982bb35aa9
viitanenlauri <lauri.a.viitanen@gmail.com>
2025-11-17 19:43:12 +0200

    nav palkki, flex väärin

--- calendar-ui/src/pages/FrontPage.jsx
+++ calendar-ui/src/pages/FrontPage.jsx
@@ -1,9 +1,11 @@
 import Calendar from './Calendar';
 import ProfileCard from './ProfileCard';
+import Navigation from '../routes/Navigation'
 
 export default function FrontPage() {
   return (
     <div className="frontpage">
+      <Navigation />
       <ProfileCard />
       <Calendar />
     </div>

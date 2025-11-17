3f5774bfcdc8b5cfa723e903cef129947d40ce14
Viivi Siren <siren.viivi@gmail.com>
2025-11-17 17:08:56 +0200

    Lisätty pekka.png, muokattu ProfileCard.jsx ja lisätty profileCard.scss

--- calendar-ui/src/pages/ProfileCard.jsx
+++ calendar-ui/src/pages/ProfileCard.jsx
@@ -1,14 +1,63 @@
-export default function ProfileCard({ name = "John Doe", email = "john@example.com", avatar }) {
+import '../styles/ProfileCard.scss'
+import profileImage from '../assets/pekka.png'
+import { getUsername } from './calendarUtils';
+
+export default function ProfileCard({
+    name = getUsername(),
+    ssn = "120812*****",
+    OID = "000000",
+    birthday = "12.8.2012",
+    group = "7A",
+    sport = "Pesäpallo - Varala",
+    guardian = "Hulda Huoltaja",
+    guardianPhone = "+358401111111",
+    phonenumber = "+358402222222",
+    password = "**********" }) {
   return (
-    <div className="profile">
+    <div className="profile-background">
       <img 
-        className="profile-avatar" 
-        src={avatar || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8rpGyEAl2yvIk_gVGxXhOjLWH9Tis3_-SARNgAr3Pl2Qt1vkR1kmeK6A3Kj9mix3lgKFCUQ&s=10"} 
-        alt={`${name} avatar`} 
+        className="profile-image" 
+        src={profileImage} 
+        alt={`${name} profile image`} 
       />
       <div className="profile-info">
-        <h2 className="profile-name">{name}</h2>
-        <p className="profile-email">{email}</p>
+        <div className="profile-name">{name}</div>
+        <p>
+          <span className="label">Henkilötunnus</span>
+          <span className='value'>{ssn}</span>
+        </p>
+        <p>
+          <span className="label">OID</span>
+          <span className='value'>{OID}</span>
+        </p>
+        <p>
+          <span className="label">Syntymäpäivä</span>
+          <span className='value'>{birthday}</span>
+        </p>
+        <p>
+          <span className="label">Ryhmä</span>
+          <span className='value'>{group}</span>
+        </p>
+        <p>
+          <span className="label">Laji/seura</span>
+          <span className='value'>{sport}</span>
+        </p>
+        <p>
+          <span className="label">Huoltaja</span>
+          <span className='value'>{guardian}</span>
+        </p>
+        <p>
+          <span className="label">Huoltajan puhelinnumero</span>
+          <span className='value'>{guardianPhone}</span>
+        </p>
+        <p>
+          <span className="label">Puhelinnumero</span>
+          <span className='value'>{phonenumber}</span>
+        </p>
+        <p>
+          <span className="label">Salasana</span>
+          <span className='value'>{password}</span>
+        </p>
       </div>
     </div>
   );
